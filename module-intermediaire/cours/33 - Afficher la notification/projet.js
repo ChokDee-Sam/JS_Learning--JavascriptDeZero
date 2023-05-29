12f62fbb72b192c13b4f956fbb51cf689e4bca9f
ChokDee-Sam <chokdee.sam@hotmail.com>
2023-05-29 14:49:31 +0700

    Commit 31 - Module Intermédiaire : Projet #1 : Compteur de clics [Comment afficher temporairement une notification]

--- module-intermediaire/cours/33 - Afficher la notification/projet.js
+++ module-intermediaire/cours/33 - Afficher la notification/projet.js
@@ -3,17 +3,20 @@
 // Visite du CSS : classes .notification et .afficher
 
 /* Comment ajouter la classe afficher ? */
-const boutonMoins = document.querySelector('#bouton-moins');
-const notification = document.querySelector('#notification');
+const boutonMoins = document.querySelector("#bouton-moins");
+const notification = document.querySelector("#notification");
 
-boutonMoins.addEventListener('click', function() {
-  
-});
+// boutonMoins.addEventListener('click', function() {
+//   notification.classList.add('afficher')
+// });
 
 /* Comment exécuter du code JavaScript après un certain temps ? */
-// boutonMoins.addEventListener('click', function() {
-//   notification.classList.add('afficher');
-//   // Après 3 secondes, retirer la classe afficher
-// });
+boutonMoins.addEventListener("click", function () {
+  notification.classList.add("afficher");
+  // Après 3 secondes, retirer la classe afficher
+  window.setTimeout(() => {
+    notification.classList.remove("afficher");
+  }, 1000);
+});
 
 /* Prochain exercice */
