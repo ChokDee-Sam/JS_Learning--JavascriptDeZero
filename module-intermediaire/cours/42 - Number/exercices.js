cdeddf805d7db3b77c0974329440b6482e7a9b3b
ChokDee-Sam <chokdee.sam@hotmail.com>
2023-06-09 22:25:43 +0700

    Commit 56 - Module Intermédiaire : Les types d'objets standards en JavaScript [Number : Exercices finaux (2)]

--- module-intermediaire/cours/42 - Number/exercices.js
+++ module-intermediaire/cours/42 - Number/exercices.js
@@ -148,3 +148,76 @@ function encodeur(message) {
 
 // Exercice 2 — Panier de fruits
 // Créez la fonction totalPanier(commandes) ci-dessous.
+function totalPanier(commandes) {
+  // On retiendra l'ordre des fruits dans ce tableau, on y stockera
+  // chaque nom de fruit (une seule fois) rencontré dans les commandes
+  let ordreDesFruits = [];
+
+  // Chaque somme de fruits sera stockée dans un objet. Le nom
+  // du fruit sera le nom de la propriété dont la valeur sera la
+  // quantité totale des commandes de ce fruit.
+  // Par exemple : sommeCommandes = { "pommes": 20, "cerises": 12 }
+  let sommeCommandes = {};
+
+  // On découpe chaque énumération de fruit des commandes...
+  let commandeParFruit = commandes.split(",");
+
+  // Pour chaque commande de fruit...
+  for (let i = 0; i < commandeParFruit.length; i++) {
+    // On extrait la quantité et le nom du fruit
+    // commandeParFruit[i] vaut par exemple "45 pommes"
+    let quantiteEtNom = commandeParFruit[i].split(" ");
+
+    // quantiteEtNom vaut ["45", "pommes"]
+    let quantite = quantiteEtNom[0];
+    let nom = quantiteEtNom[1];
+
+    // Etape 1 : on sauvegarde ce nouveau fruit dans ordreDesFruits.
+    // Si le nom de ce fruit n'est pas contenu dans notre tableau
+    // ordreDesFruits, alors on l'ajoute
+    if (!ordreDesFruits.includes(nom)) {
+      ordreDesFruits.push(nom);
+    }
+
+    // Etape 2 : on ajoute la quantité de cette commande à la
+    // quantité de fruits déjà connue. Cas particulier : si c'est
+    // la 1ère fois qu'on voit ce fruit, il faut l'ajouter à notre
+    // objet sommeCommandes.
+
+    // On vérifie si on a déjà vu ce fruit. Quand on essaye
+    // d'accéder à une propriété qui n'existe pas sur un objet, ça
+    // renvoie undefined.
+    // Si on n'a jamais enregistré ce fruit dans sommeCommandes...
+    if (sommeCommandes[nom] === undefined) {
+      // ... on l'ajoute à l'objet sommeCommandes avec sa quantité
+      // initiale. Attention quantite est une chaîne de caractères,
+      // il faut la convertir en nombre
+      sommeCommandes[nom] = Number(quantite);
+    } else {
+      // sinon, si on a déjà vu ce fruit, on ajoute la nouvelle
+      // quantité à la somme déjà existante avec l'opérateur '+='.
+      // Attention quantite est une chaîne de caractères, il faut
+      // la convertir en nombre pour faire l'addition
+      sommeCommandes[nom] += Number(quantite);
+    }
+  }
+
+  // Etape 3, on affiche dans l'ordre toutes les sommes, pour ça
+  // on utilise notre ordreDesFruits pour récupérer chaque nom
+  // de fruit à utiliser sur sommeCommandes.
+  let resultat = [];
+
+  // Pour chaque fruit dans l'ordre...
+  for (let i = 0; i < ordreDesFruits.length; i++) {
+    let nomDuFruit = ordreDesFruits[i];
+
+    // On ne doit afficher que les fruits dont la quantité est > 0
+    if (sommeCommandes[nomDuFruit] > 0) {
+      // On ajoute au tableau résultat par exemple "45 pommes"
+      resultat.push(`${sommeCommandes[nomDuFruit]} ${nomDuFruit}`);
+    }
+  }
+
+  // Etape 4 : on retourne la chaîne de caractères finale en CSV
+  return resultat.join(",");
+}
