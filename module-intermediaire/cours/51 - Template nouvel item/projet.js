c1222fe4e1dd406172764e2f00a41aefeebf74be
ChokDee-Sam <chokdee.sam@hotmail.com>
2023-06-11 20:54:14 +0700

    Commit 63 - Module Intermédiaire : Projet 2 (Liste de courses) [Template nouvel item : correction]

--- module-intermediaire/cours/51 - Template nouvel item/projet.js
+++ module-intermediaire/cours/51 - Template nouvel item/projet.js
@@ -1,33 +1,59 @@
 /* Rédigez votre code ci-dessous */
 
-// Récupérer des références
-const elTemplate = document.querySelector("#template-item");
-const liste = document.querySelector("#liste");
+// Récupérer une référence sur le template
+const elTemplateItem = document.querySelector("#template-item");
+const elNouvelItem = document.querySelector("#nouvel-item");
+const elListe = document.querySelector("#liste");
+
+// Détecter la soumission du formulaire
 const elForm = document.querySelector("form");
 
 elForm.addEventListener("submit", function (e) {
   // On empêche le rechargement de la page
   e.preventDefault();
 
-  // Cloner le li
-  const elLi = elTemplate.content.cloneNode(true);
+  // Créer un élément <li> à partir du template
+  const elLi = elTemplateItem.content.cloneNode(true);
 
-  // Ajouter le li dans la liste ul
-  liste.appendChild(elLi);
+  // Récupérer la valeur de l'input nouvel item
+  const nomItem = elNouvelItem.value;
 
-  // Récupérer la valeur de l'input
-  let nouvelItem = document.querySelector("#nouvel-item");
+  // Injecter cette valeur dans l'élément <li>
+  // Sélectionner l'élément nom <p>
+  const elNom = elLi.querySelector(".nom");
+  elNom.textContent = nomItem;
 
-  // localiser l'emplacement
-  let nouvelleListe = document.querySelector("#liste>li:last-child>p");
-
-  // injecter cette valeur dans l'element li
-  nouvelleListe.innerHTML = nouvelItem.value;
-
-  // Reset le champ d'input 
-  nouvelItem.value = "";
-
-  // Remettre le focus sur l'input
-  nouvelItem.focus();
+  // Ajouter l'élément <li> dans la liste <ul>
+  liste.append(elLi);
 });
 
+// Récupérer des références
+// const elTemplate = document.querySelector("#template-item");
+// const liste = document.querySelector("#liste");
+// const elForm = document.querySelector("form");
+
+// elForm.addEventListener("submit", function (e) {
+//   // On empêche le rechargement de la page
+//   e.preventDefault();
+
+//   // Cloner le li
+//   const elLi = elTemplate.content.cloneNode(true);
+
+//   // Ajouter le li dans la liste ul
+//   liste.appendChild(elLi);
+
+//   // Récupérer la valeur de l'input
+//   let nouvelItem = document.querySelector("#nouvel-item");
+
+//   // localiser l'emplacement
+//   let nouvelleListe = document.querySelector("#liste>li:last-child>p");
+
+//   // injecter cette valeur dans l'element li
+//   nouvelleListe.innerHTML = nouvelItem.value;
+
+//   // Reset le champ d'input
+//   nouvelItem.value = "";
+
+//   // Remettre le focus sur l'input
+//   nouvelItem.focus();
+// });
