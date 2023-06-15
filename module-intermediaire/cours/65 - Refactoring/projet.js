27ed77e7fae08270c6956e6816be1b6301f2b9b0
ChokDee-Sam <chokdee.sam@hotmail.com>
2023-06-15 21:11:12 +0700

    Commit 70 - Module Intermédiaire : Projet 2 (Liste de courses) [Refactoring + Reduire la maintenance]

--- module-intermediaire/cours/65 - Refactoring/projet.js
+++ module-intermediaire/cours/65 - Refactoring/projet.js
@@ -1,101 +1,109 @@
 /* Rédigez votre code ci-dessous */
-// Récupérer une référence sur le template
-const elTemplateItem = document.querySelector('#template-item');
-const elNouvelItem = document.querySelector('#nouvel-item');
-const elListe = document.querySelector('#liste');
+const elTemplateItem = document.querySelector("#template-item");
+const elListe = document.querySelector("#liste");
+const elNouvelItem = document.querySelector("#nouvel-item");
 
-// Détecter la soumission du formulaire
-const elForm = document.querySelector('form');
+const elForm = document.querySelector("form");
 
-// Récupérer les valeurs par défaut de quantité et unité
-const q = elTemplateItem.content.querySelector('.quantite');
+const q = elTemplateItem.content.querySelector(".quantite");
+const u = elTemplateItem.content.querySelector(".unite");
 const DEFAUT_QUANTITE = Number(q.textContent);
-const u = elTemplateItem.content.querySelector('.unite');
 const DEFAUT_UNITE = u.selectedOptions[0].value;
 
-elForm.addEventListener('submit', function(e) {
-  // On empêche le rechargement de la page
-  e.preventDefault();
-  
-  // Créer un élément <li> à partir du template
-  const elLi = elTemplateItem.content.cloneNode(true);
+// --------------------------
+// --------------------------
+// Function au Submit
+// --------------------------
+// --------------------------
 
-  // Récupérer la valeur de l'input nouvel item
+elForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const elLi = elTemplateItem.content.cloneNode(true);
   let nomItem = elNouvelItem.value;
 
-  // Supprimer les espaces en trop avant et après le nom
   nomItem = nomItem.trim();
-
-  // Supprimer les espaces d'affilés en trop
   nomItem = nomItem.replace(/ {2,}/g, " ");
 
-  // Insertion intelligente
-  // <nom>
-  // <quantité> <nom>
-  // <quantité> <unité> <nom>
-  // Est-ce que le 1er mot est un nombre ?
-  let mots = nomItem.split(' ');
+  const objetItem = extraireDonnees(nomItem);
+
+  const elNom = elLi.querySelector(".nom");
+  const elQuantite = elLi.querySelector(".quantite");
+  const elUnite = elLi.querySelector(".unite");
+
+  elNom.textContent = objetItem.nom;
+  elQuantite.textContent = objetItem.quantite;
+  elUnite.value = objetItem.unite;
+
+  liste.append(elLi);
+
+  elNouvelItem.value = "";
+
+  elNouvelItem.focus();
+});
+
+// --------------------------
+// --------------------------
+// Extraire Donné
+// --------------------------
+// --------------------------
+
+function extraireDonnees(nomItem) {
+  let mots = nomItem.split(" ");
   let premierMot = mots[0];
   let deuxiemeMot = mots[1];
-  let quantite = DEFAUT_QUANTITE;
-  let unite = DEFAUT_UNITE;
+
+  // let quantite = DEFAUT_QUANTITE;
+  // let unite = DEFAUT_UNITE;
+
+  const objetItem = {
+    nom: nomItem,
+    quantite: DEFAUT_QUANTITE,
+    unite: DEFAUT_UNITE,
+  };
 
   if (Number.isInteger(Number(premierMot))) {
-    // Si c'est une quantité, il faut l'extraire
-    // <quantité> <nom>
-    // <quantité> <unité> <nom>
-    quantite = Number(premierMot);
+    objetItem.quantite = Number(premierMot);
 
-    // Si le 2e mot est une unité, l'extraire
-    const UNITES = ['u.', 'kg', 'g', 'L'];
+    const UNITES = ["u.", "kg", "g", "L"];
+
     if (UNITES.includes(deuxiemeMot)) {
-      unite = deuxiemeMot;
-      nomItem = mots.slice(2).join(' ');
+      objetItem.unite = deuxiemeMot;
+      objetItem.nom = mots.slice(2).join(" ");
     } else {
-      // Sinon c'est que le nom commence à partir du 2e mot
-      nomItem = mots.slice(1).join(' ');
+      objetItem.nom = mots.slice(1).join(" ");
     }
   }
 
-  // Mettre la 1ère lettre en majuscule
-  // pommes => P + ommes
-  nomItem = nomItem[0].toUpperCase() + nomItem.slice(1);
+  objetItem.nom = objetItem.nom[0].toUpperCase() + objetItem.nom.slice(1);
+  return objetItem;
+}
 
-  // Injecter cette valeur dans l'élément <li>
-  // Sélectionner l'élément nom <p>
-  const elNom = elLi.querySelector('.nom');
-  const elQuantite = elLi.querySelector('.quantite');
-  const elUnite = elLi.querySelector('.unite');
-  
-  elNom.textContent = nomItem;
-  elQuantite.textContent = quantite;
-  elUnite.value = unite;
+// --------------------------
+// --------------------------
+// Les messages d'erreurs
+// --------------------------
+// --------------------------
 
-  // Ajouter l'élément <li> dans la liste <ul>
-  liste.append(elLi);
-
-  // Vider le champ nouvel item
-  elNouvelItem.value = "";
-
-  // Mettre le focus immédiatement sur le champ nouvel item
-  elNouvelItem.focus();
-  
-});
-
-elNouvelItem.addEventListener('input', function(e) {
-  elNouvelItem.setCustomValidity('');
+elNouvelItem.addEventListener("input", function (e) {
+  elNouvelItem.setCustomValidity("");
   elNouvelItem.checkValidity();
 });
 
-elNouvelItem.addEventListener('invalid', function(e) {
+elNouvelItem.addEventListener("invalid", function (e) {
   const nom = elNouvelItem.value;
 
   if (nom.length === 0) {
-    elNouvelItem.setCustomValidity("Vous devez indiquer les informations de l'item, exemple : 250 g chocolat");
+    elNouvelItem.setCustomValidity(
+      "Vous devez indiquer les informations de l'item, exemple : 250 g chocolat"
+    );
   } else if (!/[A-Za-z]{2}/.test(nom)) {
-    // Si nom ne contient pas 2 lettres côte à côte
-    elNouvelItem.setCustomValidity("Le nom de l'item doit faire 2 lettres minimum");
+    elNouvelItem.setCustomValidity(
+      "Le nom de l'item doit faire 2 lettres minimum"
+    );
   } else {
-    elNouvelItem.setCustomValidity("Les caractères spéciaux, les accents et autres lettres spécifiques ne sont pas autorisés");
+    elNouvelItem.setCustomValidity(
+      "Les caractères spéciaux, les accents et autres lettres spécifiques ne sont pas autorisés"
+    );
   }
 });
--- module-intermediaire/cours/66 - Reduire la maintenance/projet.js
+++ module-intermediaire/cours/66 - Reduire la maintenance/projet.js
@@ -1,22 +1,28 @@
 /* Rédigez votre code ci-dessous */
 // Récupérer une référence sur le template
-const elTemplateItem = document.querySelector('#template-item');
-const elNouvelItem = document.querySelector('#nouvel-item');
-const elListe = document.querySelector('#liste');
+const elTemplateItem = document.querySelector("#template-item");
+const elNouvelItem = document.querySelector("#nouvel-item");
+const elListe = document.querySelector("#liste");
 
 // Détecter la soumission du formulaire
-const elForm = document.querySelector('form');
+const elForm = document.querySelector("form");
 
 // Récupérer les valeurs par défaut de quantité et unité
-const q = elTemplateItem.content.querySelector('.quantite');
+const q = elTemplateItem.content.querySelector(".quantite");
 const DEFAUT_QUANTITE = Number(q.textContent);
-const u = elTemplateItem.content.querySelector('.unite');
+const u = elTemplateItem.content.querySelector(".unite");
 const DEFAUT_UNITE = u.selectedOptions[0].value;
 
-elForm.addEventListener('submit', function(e) {
+const UNITES = [];
+for (let i = 0; i < u.options.length; i++) {
+  UNITES.push(u.options[i].value);
+}
+console.log(UNITES);
+
+elForm.addEventListener("submit", function (e) {
   // On empêche le rechargement de la page
   e.preventDefault();
-  
+
   // Créer un élément <li> à partir du template
   const elLi = elTemplateItem.content.cloneNode(true);
 
@@ -33,10 +39,10 @@ elForm.addEventListener('submit', function(e) {
 
   // Injecter cette valeur dans l'élément <li>
   // Sélectionner l'élément nom <p>
-  const elNom = elLi.querySelector('.nom');
-  const elQuantite = elLi.querySelector('.quantite');
-  const elUnite = elLi.querySelector('.unite');
-  
+  const elNom = elLi.querySelector(".nom");
+  const elQuantite = elLi.querySelector(".quantite");
+  const elUnite = elLi.querySelector(".unite");
+
   elNom.textContent = objetItem.nom;
   elQuantite.textContent = objetItem.quantite;
   elUnite.value = objetItem.unite;
@@ -49,7 +55,6 @@ elForm.addEventListener('submit', function(e) {
 
   // Mettre le focus immédiatement sur le champ nouvel item
   elNouvelItem.focus();
-  
 });
 
 // Insertion intelligente
@@ -58,7 +63,7 @@ function extraireDonnees(nomItem) {
   // <quantité> <nom>
   // <quantité> <unité> <nom>
   // Est-ce que le 1er mot est un nombre ?
-  let mots = nomItem.split(' ');
+  let mots = nomItem.split(" ");
   let premierMot = mots[0];
   let deuxiemeMot = mots[1];
 
@@ -66,7 +71,7 @@ function extraireDonnees(nomItem) {
     nom: nomItem,
     quantite: DEFAUT_QUANTITE,
     unite: DEFAUT_UNITE,
-  }
+  };
 
   if (Number.isInteger(Number(premierMot))) {
     // Si c'est une quantité, il faut l'extraire
@@ -75,13 +80,12 @@ function extraireDonnees(nomItem) {
     objetItem.quantite = Number(premierMot);
 
     // Si le 2e mot est une unité, l'extraire
-    const UNITES = ['u.', 'kg', 'g', 'L'];
     if (UNITES.includes(deuxiemeMot)) {
       objetItem.unite = deuxiemeMot;
-      objetItem.nom = mots.slice(2).join(' ');
+      objetItem.nom = mots.slice(2).join(" ");
     } else {
       // Sinon c'est que le nom commence à partir du 2e mot
-      objetItem.nom = mots.slice(1).join(' ');
+      objetItem.nom = mots.slice(1).join(" ");
     }
   }
 
@@ -92,20 +96,26 @@ function extraireDonnees(nomItem) {
   return objetItem;
 }
 
-elNouvelItem.addEventListener('input', function(e) {
-  elNouvelItem.setCustomValidity('');
+elNouvelItem.addEventListener("input", function (e) {
+  elNouvelItem.setCustomValidity("");
   elNouvelItem.checkValidity();
 });
 
-elNouvelItem.addEventListener('invalid', function(e) {
+elNouvelItem.addEventListener("invalid", function (e) {
   const nom = elNouvelItem.value;
 
   if (nom.length === 0) {
-    elNouvelItem.setCustomValidity("Vous devez indiquer les informations de l'item, exemple : 250 g chocolat");
+    elNouvelItem.setCustomValidity(
+      "Vous devez indiquer les informations de l'item, exemple : 250 g chocolat"
+    );
   } else if (!/[A-Za-z]{2}/.test(nom)) {
     // Si nom ne contient pas 2 lettres côte à côte
-    elNouvelItem.setCustomValidity("Le nom de l'item doit faire 2 lettres minimum");
+    elNouvelItem.setCustomValidity(
+      "Le nom de l'item doit faire 2 lettres minimum"
+    );
   } else {
-    elNouvelItem.setCustomValidity("Les caractères spéciaux, les accents et autres lettres spécifiques ne sont pas autorisés");
+    elNouvelItem.setCustomValidity(
+      "Les caractères spéciaux, les accents et autres lettres spécifiques ne sont pas autorisés"
+    );
   }
 });
