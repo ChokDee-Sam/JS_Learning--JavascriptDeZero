2fa5217f764880f5f3ea779c1cfcd33fbc8b9e56
ChokDee-Sam <chokdee.sam@hotmail.com>
2023-07-12 16:17:33 +0700

    Commit 80 - Module Avancé : On recode la méthode filter [Notes]

--- 3. module-avance/cours/14-1 - On recode filter de zero/cours.js
+++ 3. module-avance/cours/14-1 - On recode filter de zero/cours.js
@@ -1,8 +1,16 @@
 /* La méthode filter() réécrite de zéro */
-
 /* Objet initial qui jouera le rôle du tableau (Array) */
 let objetTableau = {
-  
+  donnees: ["zero", "un", "deux", "trois"],
+  filter: function (callback) {
+    let resultat = [];
+    for (let index = 0; index < this.donnees.length; index++) {
+      if (callback(this.donnees[index], index, this.donnees)) {
+        resultat.push(this.donnees[index]);
+      }
+    }
+    return resultat
+  },
 };
 
 /* Fonction de callback */
