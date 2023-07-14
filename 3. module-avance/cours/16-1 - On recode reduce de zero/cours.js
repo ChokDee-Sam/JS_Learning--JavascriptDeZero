ecfdb56325a9ef09d6e5033bf198e7d2bcb759aa
ChokDee-Sam <chokdee.sam@hotmail.com>
2023-07-14 23:39:27 +0700

    Commit 86 - Module Avancé : On recode la méthode reduce [Notes]

--- 3. module-avance/cours/16-1 - On recode reduce de zero/cours.js
+++ 3. module-avance/cours/16-1 - On recode reduce de zero/cours.js
@@ -2,10 +2,21 @@
 
 /* Objet initial qui jouera le rôle du tableau (Array) */
 let objetTableau = {
-  
+  donnees: ["zero", "un", "deux", "trois"],
+  reduce: function (callback, valeurInitiale) {
+    let resultat = valeurInitiale;
+
+    for (let index = 0; index < this.donnees.length; index++) {
+      resultat = callback(resultat, this.donnees[index], index, this.donnees);
+    }
+    return resultat;
+  },
 };
 
+//-------------------------------------------------
 /* Fonction de callback */
+//-------------------------------------------------
+
 function concatenation(resultat, chaine) {
   let chaineFinale = resultat + chaine;
   return chaineFinale;
@@ -14,3 +25,34 @@ function concatenation(resultat, chaine) {
 // On combine notre "tableau"
 let resultat = objetTableau.reduce(concatenation, "");
 console.log(resultat);
+
+//-------------------------------------------------
+// Dans le cas où on ne donne pas de valeur initiale
+//-------------------------------------------------
+
+// Objet initial qui jouera le rôle du tableau (Array) */
+let objetTableauSansValeurInitiale = {
+  donnees: ["zero", "un", "deux", "trois"],
+  reduce: function (callback, valeurInitiale) {
+    let resultat = valeurInitiale;
+
+    // On ajoute un debut index qui pourrait être modifié s'il manque la valeur initiale
+    let debutIndex = 0;
+
+    // On entre notre condition
+    if (valeurInitiale === undefined) {
+      resultat = this.donnees[0]; //  pour donner une valeur initiale du premier element du tableau à l'accumulateur
+      debutIndex = 1; //  pour     let index = debutIndex      dans la boucle for
+    }
+
+    // On fait notre boucle avec notre debutIndex défini de manière à avoir ou non une valeur initiale
+    for (let index = debutIndex; index < this.donnees.length; index++) {
+      resultat = callback(resultat, this.donnees[index], index, this.donnees);
+    }
+    return resultat;
+  },
+};
+
+// On fait notre .reduce() sans définir de valeur initiale
+let autreResultat = objetTableauSansValeurInitiale.reduce(concatenation);
+console.log(autreResultat);
